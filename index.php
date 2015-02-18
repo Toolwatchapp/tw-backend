8753bce19d22a05fe70c7af63d6364f7f380b418
Sylvain Moreau <mor.sylvain@gmail.com>
2015-02-18 18:56:25 +0100

    Commit initial

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Toolwatch - Your watch accuracy</title>
+        <meta name="keywords" content="toolwatch, accuracy, precision, measure, mechanical watch, manual winding, automatic winding">
+        <meta name="description" content="Easily measure and track the accuracy of your mechanical watch">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://apis.google.com/_/scs/apps-static/_/js/k=oz.gapi.fr.R4yFenW-hCo.O/m=gapi_iframes_style_bubble/exm=auth,plusone/rt=j/sv=1/d=1/ed=1/am=AQ/rs=AGLTcCNAQxLbdjVq0ebFm0Z7mGgqTewDvA/t=zcms/cb=gapi.loaded_2" async=""></script>
+        <script src="https://apis.google.com/_/scs/apps-static/_/js/k=oz.gapi.fr.R4yFenW-hCo.O/m=auth/exm=plusone/rt=j/sv=1/d=1/ed=1/am=AQ/rs=AGLTcCNAQxLbdjVq0ebFm0Z7mGgqTewDvA/t=zcms/cb=gapi.loaded_1" async=""></script>
+        <script src="https://apis.google.com/_/scs/apps-static/_/js/k=oz.gapi.fr.R4yFenW-hCo.O/m=plusone/rt=j/sv=1/d=1/ed=1/am=AQ/rs=AGLTcCNAQxLbdjVq0ebFm0Z7mGgqTewDvA/t=zcms/cb=gapi.loaded_0" async=""></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>
+    <head>
+    <body>
+    
+    </body>
+</html>
