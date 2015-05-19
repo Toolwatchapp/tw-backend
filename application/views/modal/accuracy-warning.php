3ed9e7f00d3f09813055467d009bdff3b0567f77
mathieu <mathieu.nayrolles@gmail.com>
2015-05-19 12:50:41 -0400

    Forgot the new view. Refs #10

---
+++ application/views/modal/accuracy-warning.php
@@ -0,0 +1,6 @@
+<center>
+    <h1>12 hours limit!</h1>
+    <p>In order for the measure to be precise, you must wait at least 12 hours before checking back your measure. Don't worry, we will send you an email to remind you. In the meantime, enjoy wearing your timepiece !</p>
+    <br>
+    <p>Happy toolwatching!</p>
+</center>
