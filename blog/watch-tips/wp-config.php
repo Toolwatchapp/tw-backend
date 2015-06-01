521144fe34df2c97848010bb03d35ef860fd750f
mathieu <mathieu.nayrolles@gmail.com>
2015-06-01 13:43:20 -0400

    Change database settings for stage wordpress. Refs #25

--- blog/watch-tips/wp-config.php
+++ blog/watch-tips/wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'toolwatch');
+define('DB_NAME', 'heroku_569fb1a070c9a16');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'bf5857955eb8a1');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'f5e774f8');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'us-cdbr-iron-east-02.cleardb.net');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
