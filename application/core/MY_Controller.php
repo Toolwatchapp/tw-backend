c1f9ff67307c89e66d3fc3de8c027d0486c0472d
mathieu <mathieu.nayrolles@gmail.com>
2015-07-07 14:40:04 -0400

    Preprod is now hidden from outside world. #21

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -10,12 +10,25 @@ class MY_Controller extends CI_Controller
 	public function __construct()
 	{
 		parent::__construct();
+
         $this->_headerData['userIsLoggedIn'] = $this->user->isLoggedIn();
 		$this->_headerData['styleSheets'] = array('main');
 		$this->_headerData['javaScripts'] = array('jquery.min', 'bootstrap.min', 'application', 'MediaElement/mediaelement-and-player.min', 
 			'facebook');
         $this->_headerData['headerClass'] = '';
-        
+
+
+        if(!$this->user->isAdmin()){
+        	 if(strpos(base_url(), "tw-preprod") !== false && !$this->input->post('pw')){
+			echo "<form action='".base_url()."' method='post'><input name='pw' type='password'/><input type='submit'/>";
+				die;
+			}else if($this->input->post('pw') && $this->input->post('pw') === '&BSdJ88{waHK!Zj'){
+				$this->session->set_userdata('admin', 'true');
+			}else{
+				die;
+			}
+        }
+
         if($this->_needLoggedIn && !$this->user->isLoggedIn())
         {
             redirect(base_url());   
