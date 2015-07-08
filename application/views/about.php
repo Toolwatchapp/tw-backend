cb1a6bae10836bdcb8411f48570fc23b53c2b272
mathieu <mathieu.nayrolles@gmail.com>
2015-07-08 16:48:52 -0400

    add some margins on description. #39

--- application/views/about.php
+++ application/views/about.php
@@ -37,7 +37,7 @@
             <div class="row">
                 <div class="col-md-4 name"><center><div class="fa fa-user"></div><br>Marc<br>Founder</center></div>
                 <div class="col-md-8 infos">
-                    <p>Marc is passionate, aficionado or even addicted to mechanical watches. We cannot even use a superlative anymore. He knew how to read hours before walking! Now in the watch valley, he gives all his extra time to Toolwatch.io and to all the watch enthusiasts around the world. He loves watches, clocks, and also digital product!</p>
+                    <p style="margin: 5px;">Marc is passionate, aficionado or even addicted to mechanical watches. We cannot even use a superlative anymore. He knew how to read hours before walking! Now in the watch valley, he gives all his extra time to Toolwatch.io and to all the watch enthusiasts around the world. He loves watches, clocks, and also digital product!</p>
                 </div>
             </div>
         </div>
@@ -47,7 +47,7 @@
             <div class="row">
                 <div class="col-md-4 name"><center><div class="fa fa-user"></div><br>Vincent<br>Co-founder</center></div>
                 <div class="col-md-8 infos">
-                   <p>Vincent is on time. His watches are actually so accurate that he could replace the atomic clock. Passionate about precision, he loves mechanical watches but he also likes doing business with his peers. Currently in Geneva, he navigates inside the watch industry and also makes his passion alive with Toolwatch.io!</p>
+                   <p  style="margin: 5px;">Vincent is on time. His watches are actually so accurate that he could replace the atomic clock. Passionate about precision, he loves mechanical watches but he also likes doing business with his peers. Currently in Geneva, he navigates inside the watch industry and also makes his passion alive with Toolwatch.io!</p>
                 </div>
             </div>
         </div>
@@ -57,7 +57,7 @@
             <div class="row">
                 <div class="col-md-4 name"><center><div class="fa fa-user"></div><br>Mathieu<br>Co-founder</center></div>
                 <div class="col-md-8 infos">
-                    <p>Mathieu, the last but not least of the adventure, is the master of development. He is the type of man wearing at the same time both a smart and a mechanical watch. However he is still wondering which one is the most useful..(we haven’t told him yet!). Trying to survive in Canada, he has now reached higher latitude to quietly develop his passion for <s>computer</s> watches!</p>
+                    <p  style="margin: 5px;">Mathieu, the last but not least of the adventure, is the master of development. He is the type of man wearing at the same time both a smart and a mechanical watch. However he is still wondering which one is the most useful..(we haven’t told him yet!). Trying to survive in Canada, he has now reached higher latitude to quietly develop his passion for <s>computer</s> watches!</p>
                 </div>
             </div>
         </div>
