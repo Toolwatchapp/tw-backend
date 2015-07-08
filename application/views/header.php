9443e1f40ceaab80e9bfb561688e573978db135d
mathieu <mathieu.nayrolles@gmail.com>
2015-07-08 16:09:05 -0400

    Revert "add bounce bar script #43"
    This reverts commit 2a904471256cc1b5aafbd9d0d95e705b24cb9925.

--- application/views/header.php
+++ application/views/header.php
@@ -40,7 +40,6 @@
           ga('create', 'UA-59148878-1', 'auto');
           ga('send', 'pageview');
     </script>
-    <script type="text/javascript" src="//cdn.bounce.bar/114697.js"></script>
     <!--[if lt IE 8]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
