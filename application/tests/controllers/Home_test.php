30a2e10d38c385fcd485cbf1bda7fc558b06c0e2
mathieu <mathieu.nayrolles@gmail.com>
2015-08-10 18:14:19 -0400

    add test for Home controller #41

---
+++ application/tests/controllers/Home_test.php
@@ -0,0 +1,42 @@
+<?php
+
+class Home_test extends TestCase {
+
+	public function test_index() {
+		$output = $this->request('GET', ['Home', 'index']);
+		$this->assertContains('class="home-intro"', $output);
+		$this->assertContains('class="home-picto"', $output);
+		$this->assertContains('id="demo-screen"', $output);
+		$this->assertContains('id="mosa-screen"', $output);
+		$this->assertContains('<footer>', $output);
+	}
+
+	public function test_result() {
+		$output = $this->request('GET', ['Home', 'result']);
+		$this->assertContains('accuracy.jpg', $output);
+	}
+
+	public function test_about() {
+		$output = $this->request('GET', ['Home', 'about']);
+		$this->assertContains('<title>Toolwatch • About Toolwatch</title>', $output);
+	}
+
+	public function test_contact() {
+		$output = $this->request('GET', ['Home', 'contact']);
+		$this->assertContains('<title>Toolwatch • Contact</title>', $output);
+		$this->assertContains('<form class="form-horizontal" name="contact">', $output);
+	}
+
+	public function test_signupEmail() {
+
+		$output = $this->request('GET', ['Home', 'signupEmail']);
+
+		$this->assertContains('<div style="margin-top: 20px; font-family:', $output);
+	}
+
+	public function test_resetPassword() {
+		$output = $this->request('GET', ['Home', 'resetPassword']);
+		$this->assertContains('<div class="col-md-12"><center><h1>Reset your password</h1></center></div>', $output);
+	}
+
+}
