ee20e37db931bc310aaca486bfd0c558be575987
Mathieu N <mathieu.nayrolles@gmail.com>
2015-11-24 06:51:17 -0500

    Add email in autoloaded model #35

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -131,4 +131,4 @@ $autoload['language'] = array();
 |	$autoload['model'] = array('first_model' => 'first');
  */
 
-$autoload['model'] = array('user');
+$autoload['model'] = array('user', 'email');
