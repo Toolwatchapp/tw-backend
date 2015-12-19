36f78344ab8724b12fd454fb3a7db65c3ca1941e
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2015-12-19 04:04:22 -0500

    commenting + refactoring

--- application/models/Measure.php
+++ application/models/Measure.php
@@ -1,14 +1,48 @@
 <?php if (!defined('BASEPATH')) {exit('No direct script access allowed');
 }
 
+/**
+ * Measure model.
+ *
+ * Measure model is responsible for all measure related transactions.
+ * Also, measure model extends ObservableModel in order to send
+ * information about transactions to attached observers.
+ */
 class Measure extends ObservableModel {
 
+	/**
+	 * Default constructeur
+	 */
 	function __construct() {
 		parent::__construct();
 		$this->table_name = "measure";
+
+		//The computeAccuracy method will be executed
+		//with each row of any fetch from the database.
+		//I decided to do this (instead of storing the field in the database,
+		//for example) because, if the formulae changed, we can ship a new
+		//version and provide new computation of the base materials
+		//(times of measure).
+		//
+		//The downsides are that we compute the accuracy each time the user
+		//requests each dashboard and database exports are painfull if you want
+		//the accuracy as you have to make sure that the formulae of the db export
+		//matches this one.
 		$this->after_find = array('computeAccuracy');
 	}
 
+	/**
+	 * Get the last measure of each $userWatches
+	 *
+	 * TODO: I really don't like the overall look of this method.
+	 * We should return an array of $watchMeasure and get rid of $dataPushing.
+	 * I think data pushing date back from when measures 1/2 and 2/2 were not
+	 * a couple but two instances of the same db objects.
+	 *
+	 * @param  int $userId      id of the user
+	 * @param  array $userWatches watches of the user
+	 * @return array The last measure per $userWatches for $userId
+	 */
 	function getMeasuresByUser($userId, $userWatches) {
 
 		$data        = array();
@@ -35,7 +69,7 @@ class Measure extends ObservableModel {
 					foreach ($watchMeasures as $watchMeasure) {
 						//Compute accuracy
 						if ($watchMeasure->statusId == 2) {
-							$data[$dataPushing]['accuracy'] = $watchMeasure->accuracy;//sprintf("%.1f", $this->computeAccuracy($watchMeasure));
+							$data[$dataPushing]['accuracy'] = $watchMeasure->accuracy;
 							$data[$dataPushing]['statusId'] = $watchMeasure->statusId;
 							//Check if the measure was made less than 12 hours ago
 						} else if (((time()-$watchMeasure->measureReferenceTime)/3600) < 12) {
@@ -65,15 +99,29 @@ class Measure extends ObservableModel {
 		return $data;
 	}
 
+	/**
+	 * Compute the accuracy of a watch given the raw data of the database
+	 * @param  Measure $watchMeasure A watchMeasure object containing row data
+	 * about the timing of measure
+	 */
 	public function computeAccuracy(&$watchMeasure) {
 		$userDelta = $watchMeasure->accuracyUserTime-$watchMeasure->measureUserTime;
 		$refDelta  = $watchMeasure->accuracyReferenceTime-$watchMeasure->measureReferenceTime;
 		$accuracy  = ($userDelta*86400/$refDelta)-86400;
 		$accuracy  = sprintf("%.1f", $accuracy);
 		$watchMeasure->accuracy = $accuracy;
-		return $watchMeasure;
 	}
 
+	/**
+	 * Add a base measure (1/2) to $watchId given $referenceTime and $userTime
+	 *
+	 * All previous measures, completed or not, will be archived (status = 3)
+	 * at the creation of a new measure.
+	 *
+	 * @param int $watchId       The watch being mesured
+	 * @param Long $referenceTime the reference time in ms
+	 * @param Long $userTime      the user time in ms
+	 */
 	function addBaseMesure($watchId, $referenceTime, $userTime) {
 
 		//Archive previous measure couples
@@ -95,6 +143,16 @@ class Measure extends ObservableModel {
 		return $returnValue;
 	}
 
+	/**
+	 * Add an accuracy measure (2/2) for $measureId given $referenceTime and
+	 * $userTime
+	 *
+	 * @param [type] $measureId     [description]
+	 * @param [type] $referenceTime [description]
+	 * @param [type] $userTime      [description]
+	 *
+	 * @return mixed|boolean The new accuracy measure.
+	 */
 	function addAccuracyMesure($measureId, $referenceTime, $userTime) {
 
 		$data = array(
@@ -104,8 +162,7 @@ class Measure extends ObservableModel {
 
 		if ($this->update($measureId, $data) !== false) {
 
-			$watchMeasure           = $this->find($measureId);
-			//$watchMeasure->accuracy = $this->computeAccuracy($watchMeasure);
+			$watchMeasure = $this->find($measureId);
 
 			$this->notify(NEW_ACCURACY,
 				array('measure'   => $watchMeasure));
@@ -117,6 +174,12 @@ class Measure extends ObservableModel {
 
 	}
 
+	/**
+	 * (Soft) Delete the $measureId measure.
+	 *
+	 * @param  int $measureId MeasureId of the measure to be deleted
+	 * @return boolean
+	 */
 	function deleteMesure($measureId) {
 
 		$data = array('statusId' => 4);
@@ -128,6 +191,12 @@ class Measure extends ObservableModel {
 		return $this->update($measureId, $data) !== false;
 	}
 
+	/**
+	 * Count the amount of watch of $watchBrand
+	 *
+	 * @param  String $watchBrand The watchBrand of interest
+	 * @return int How many watches belong tp $watchBrand
+	 */
 	function getMeasuresCountByWatchBrand($watchBrand) {
 		return $this->select("count(1) as cnt")
 		            ->join("watch", "watch.watchId = measure.watchId")
