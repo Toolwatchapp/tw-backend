abc6e04a4ec927293a272f1cd4bec45f0ba74295
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-01-06 13:10:21 -0500

    insert obvious php error to check heroku error reporting

--- index.php
+++ index.php
@@ -67,6 +67,7 @@ switch (ENVIRONMENT) {
 	case 'development':
 		error_reporting(E_ALL&~E_NOTICE&~E_DEPRECATED&~E_STRICT&~E_USER_NOTICE&~E_USER_DEPRECATED);
 		ini_set('display_errors', 1);
+		va
 		break;
 
 	case 'testing':
