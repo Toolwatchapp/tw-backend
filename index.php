9640ac12c36b831cd98d202812428da26d191851
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-01-06 13:08:42 -0500

    change error reporting

--- index.php
+++ index.php
@@ -65,7 +65,6 @@ define('ENVIRONMENT', 'development');
  */
 switch (ENVIRONMENT) {
 	case 'development':
-		error_reporting(-1);
 		error_reporting(E_ALL&~E_NOTICE&~E_DEPRECATED&~E_STRICT&~E_USER_NOTICE&~E_USER_DEPRECATED);
 		ini_set('display_errors', 1);
 		break;
