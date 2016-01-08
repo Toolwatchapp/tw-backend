e01b3548425502c3cf5a538408c392bee714d800
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-01-08 15:02:24 -0500

    Fix time css #98

--- application/views/time.php
+++ application/views/time.php
@@ -1,44 +1,27 @@
-<div style="margin-top:30px;" class="row">
+<div style="margin-top:40px;" class="row">
     <div class="col-md-12"><div class="line black"></div></div>
 </div>
-<div class="row">
+<div id="clockdiv" class="row">
 
-    <div id="demo" class="col-sm-12">
-
-       <div class="col-sm-3" >
-            <canvas id="canvas_animated_watch" width="250" height="250"></canvas>
-            <img style="display:none" id="watch" src="<?php echo img_url('flatwatch-blank.png');?>">
-       </div>
-
-       <div id="clockdiv" class="col-sm-offset-1 col-sm-8">
-         <div class="row">
-
-           <div class="col-sm-12">
+           <div class="col-sm-4">
+             <br>
              <span class="days"></span>
-             <span class="months"></span>,
+             <span class="months"></span><br />
              <span class="years"></span>
            </div>
 
-         </div>
+           <div class="col-sm-4" >
+                <canvas id="canvas_animated_watch" width="250" height="250"></canvas>
+                <img style="display:none" id="watch" src="<?php echo img_url('flatwatch-blank.png');?>">
+           </div>
 
-         <div class="row">
 
-           <div class="col-sm-offset-1 col-sm-7">
-
+           <div class="col-sm-4">
+             <br>
              <span class="hours"></span>
-             :<span class="minutes"></span>
-             :<span class="seconds"></span>
-
+             : <span class="minutes"></span>
+             : <span class="seconds"></span>
+             <br />
+            <img style="margin-top:-5px" id="moonPhaseImage" src="<?php echo base_url();?>/assets/img/moon/" border="0" width="60px" height="100%" title="">
            </div>
-
-           <div class="col-sm-3">
-             <img style="margin-top:-5px" id="moonPhaseImage" src="<?php echo base_url();?>/assets/img/moon/" border="0" width="100%" height="100%" title="">
-           </div>
-
-         </div>
-
-       </div>
-
-    </div>
-
 </div>
