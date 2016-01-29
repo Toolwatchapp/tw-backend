61bdb7448673caa3b07fe89bcf7d70cafb3fd23c
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-01-29 17:57:41 -0500

    update meta description #108

--- application/views/header.php
+++ application/views/header.php
@@ -8,18 +8,18 @@
 
 ?></title>
     <meta name="keywords" content="toolwatch, toolwatchapp, accuracy, precision, measure, mechanical watch, manual winding, automatic winding">
-    <meta name="description" content="Toolwatch makes it super easy to measure the accuracy of any mechanical watch. Keep your watch’s accuracy at its best with Toolwatch.">
+    <meta name="description" content="<?php if (isset($meta_description)) {echo $meta_description;
+} else {
+	echo 'Toolwatch makes it super easy to measure the accuracy of a mechanical watch. Keep your watch's accuracy at its best and know if you should service your watch';
+}
+?>" />
 
     <meta property="og:title" content="<?php if (isset($title)) {echo $title;
 } else {
 	echo 'Easily measure and track the accuracy of your mechanical watch';
 }
 ?>" />
-    <meta property="og:description" content="<?php if (isset($meta_description)) {echo $meta_description;
-} else {
-	echo 'Toolwatch makes it super easy to measure the accuracy of any mechanical watch. Keep your watch’s accuracy at its best with Toolwatch.';
-}
-?>" />
+    <meta property="og:description" content="<?php echo 'Toolwatch makes it super easy to measure the accuracy of a mechanical watch. Keep your watch's accuracy at its best and know if you should service your watch' ?>" />
     <meta property="og:image" content="<?php if (isset($meta_img)) {echo $meta_img;
 } else {
 	echo img_url('share.png');
