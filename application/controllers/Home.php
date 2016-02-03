5c9ea38d2813f2863cd3dabaf89087f9d7f09cba
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-02-03 13:26:54 -0500

    Refactor Home controller

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,26 +3,23 @@
 
 class Home extends MY_Controller {
 
-	//TODO: Can we overide load view to append .mobile ?
-	private $viewName = "home/home";
+	private $viewName;
 
 	function __construct() {
 		parent::__construct();
+
 		$this->load->model('measure');
 
-		if ($this->agent->is_mobile()) {
-			$this->viewName = "home/home-mobile";
-		}
-
+		$this->viewName = $this->agent->is_mobile()
+			? "home/home-mobile"
+			: "home/home";
 	}
 
 	function index() {
 
-		if (!$this->agent->is_mobile()) {
-			array_push($this->_headerData['javaScripts'], "home.logic", "watch.animation");
-		} else {
-			array_push($this->_headerData['javaScripts'], "home.logic.mobile");
-		}
+		$this->agent->is_mobile()
+			? array_push($this->_headerData['javaScripts'], "home.logic.mobile")
+			: array_push($this->_headerData['javaScripts'], "home.logic", "watch.animation");
 
 		$this->load->view('header', $this->_headerData);
 		$this->load->view($this->viewName, $this->homeMessage());
@@ -31,17 +28,8 @@ class Home extends MY_Controller {
 
 	function result() {
 
-		if (!$this->agent->is_mobile()) {
-			array_push($this->_headerData['javaScripts'], "home.logic", "watch.animation");
-		} else {
-			array_push($this->_headerData['javaScripts'], "home.logic.mobile");
-		}
-
 		$this->_headerData["meta_img"] = img_url("accuracy.jpg");
-
-		$this->load->view('header', $this->_headerData);
-		$this->load->view($this->viewName, $this->homeMessage());
-		$this->load->view('footer');
+		$this->index();
 	}
 
 	private function homeMessage() {
@@ -55,29 +43,24 @@ class Home extends MY_Controller {
 		if (!$this->user->isLoggedIn()) {
 
 			$homePage = 'HOME_PAGE_'.$randWatches;
-
 			$this->event->add($homePage);
-
 		}
 
-		if (!$this->agent->is_mobile()) {
-			return array('title' => $this->measure
-				->getMeasuresCountByWatchBrand($watchBrands[$randBrands]).
-				' '.$watchBrands[$randBrands].' measured on Toolwatch.io',
-				'video_url' => vid_url($videos[$randWatches]).'.mp4');
-		} else {
-			return array('title' => $this->measure
-				->getMeasuresCountByWatchBrand($watchBrands[$randBrands]).
-				' '.$watchBrands[$randBrands].' measured on Toolwatch.io',
-				'video_url' => img_url($videos[$randWatches]).'.png');
-		}
+		$title = $this->measure
+			->getMeasuresCountByWatchBrand($watchBrands[$randBrands]).
+			' '.$watchBrands[$randBrands].' measured on Toolwatch.io';
 
+		$url = $this->agent->is_mobile()
+			? img_url($videos[$randWatches]).'.png'
+			: vid_url($videos[$randWatches]).'.mp4';
+
+		return array('title'=>$title, 'video_url'=>$url);
 	}
 
 	function logout() {
 
 		$this->user->logout();
-		redirect(base_url());
+		return redirect(base_url());
 	}
 
 	function resetPassword($resetToken = '') {
