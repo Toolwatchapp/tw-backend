f448076a686749478a33405019512b3338d2c58e
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-02-14 17:56:20 -0500

    Show only the resuts on the homepage #58

--- application/views/home/demo.php
+++ application/views/home/demo.php
@@ -8,22 +8,6 @@
        </div>
 
 
-        <div  id="demo-first-step" class="form-group">
-            <div class="col-sm-offset-1 col-sm-4">
-              <center>
-                <br />
-                <br />
-                <br />
-                <a
-                  style="display:none; background-color:#1763A5"
-                  id="sync-button"
-                  class="btn btn-primary  btn-lg" disabled="disabled">
-                </a>
-              </center>
-            </div>
-        </div>
-
-
         <div style="margin-top:90px;" id="demo-second-step" class="col-sm-5" >
             <div class="col-sm-12">
                 <h1>Congratulations!</h1> <br/> <p class="accuracy-subtitle"> The accuracy of your <strong><span class="watch-brand">watch</span></strong> is </p>
