c321f7ab0d246f66102d05510c8a06760a3c6880
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-02-26 14:34:29 -0500

    acceptance tests

--- application/config/database.php
+++ application/config/database.php
@@ -85,8 +85,8 @@ $db['default'] = array(
 );
 
 
-// $url                       = parse_url(getenv("TW_DB_URL"));
-// $db['default']['hostname'] = $url["host"];
-// $db['default']['username'] = $url["user"];
-// $db['default']['password'] = $url["pass"];
-// $db['default']['database'] = substr($url["path"], 1);
+$url                       = parse_url(getenv("TW_DB_URL"));
+$db['default']['hostname'] = $url["host"];
+$db['default']['username'] = $url["user"];
+$db['default']['password'] = $url["pass"];
+$db['default']['database'] = substr($url["path"], 1);
--- tests/acceptance/_bootstrap.php
+++ tests/acceptance/_bootstrap.php
@@ -1,2 +1,24 @@
 <?php
 // Here you can initialize variables that will be available to your tests
+
+$url                       = parse_url(getenv("TW_DB_URL"));
+// $db['default']['hostname'] = $url["host"];
+// $db['default']['username'] = $url["user"];
+// $db['default']['password'] = $url["pass"];
+// $db['default']['database'] = substr($url["path"], 1);
+//
+var_dump($url);
+
+\Codeception\Configuration::$defaultSuiteSettings['modules']['config'] = [
+    'Db' => [
+        'dsn' => 'mysql:host=' . $url["host"] . ';dbname=tw_ci' . substr($url["path"], 1),
+        'user' =>  $url["user"],
+        'password' => $url["pass"],
+        'dump' => '_data/dump.sql',
+        'populate' => true,
+        'cleanup' => false,
+        'reconnect' => true
+    ]
+];
+
+var_dump(\Codeception\Configuration::$defaultSuiteSettings);
