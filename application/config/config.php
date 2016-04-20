28131281e0233ede293a50bcca5a3c1a4e10b33f
Mathieu Nayrolles <MathieuNls@users.noreply.github.com>
2016-04-20 13:11:45 -0400

    Jack bot is now allowed to post without a csrf_token

--- application/config/config.php
+++ application/config/config.php
@@ -436,7 +436,7 @@ $config['csrf_token_name']   = 'csrf_test_name';
 $config['csrf_cookie_name']  = 'csrf_cookie_name';
 $config['csrf_expire']       = 7200;
 $config['csrf_regenerate']   = FALSE;
-$config['csrf_exclude_uris'] = array('api/[a-z\/]+');
+$config['csrf_exclude_uris'] = array('hooks', 'api/[a-z\/]+');
 
 /*
 |--------------------------------------------------------------------------
