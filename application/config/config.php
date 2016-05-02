3e28e1bf9e98013fb50063bc00433fabb915a669
Mathieu Nayrolles <MathieuNls@users.noreply.github.com>
2016-05-02 10:04:06 -0400

    Set session time-out to 1 year #157

--- application/config/config.php
+++ application/config/config.php
@@ -362,10 +362,10 @@ $config['encryption_key'] = 'BXdE8HoAUMSdj5ukKwww24n5BwIW1NZ5';
  */
  $config['sess_driver']             = 'database';
  $config['sess_cookie_name']        = 'ci_session';
- $config['sess_expiration']         = 1440;
+ $config['sess_expiration']         = 32140800;
  $config['sess_save_path']          = 'ci_sessions';
  $config['sess_match_ip']           = FALSE;
- $config['sess_time_to_update']     = 300;
+ $config['sess_time_to_update']     = 32140800;
  $config['sess_regenerate_destroy'] = FALSE;
 
 /*
