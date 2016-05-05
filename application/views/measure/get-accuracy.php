5f0452baa0bb74bd868c825f2b825bc518cf717b
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-05-05 19:13:01 -0400

    new result page #152

--- application/views/measure/get-accuracy.php
+++ application/views/measure/get-accuracy.php
@@ -12,7 +12,128 @@
           </div>
         </div>
     </div>
+
+
     <span id="accuracyHolder"></span>
+
+
+    <div style="display:none;" class="form-group sync-success row">
+
+      <div  class="col-md-8">
+        <center>
+
+          <h1>THANK YOU FOR MEASURING YOUR WATCH!</h1>
+          <p><br><br>You can continue enjoying this mechanical beauty on your wrist.<br><br></p>
+          <h4>DON'T KEEP THIS GOLDMINE JUST FOR YOU, SHARE IT ;)</h4>
+
+          <br />
+          <a style="padding:15px; margin-left:20px; color:white; background-color: #36528c; width:45%; " class="col-md-6 share-button"
+          href="http://www.facebook.com/sharer.php?s=100&p[url]=https://toolwatch.io&p[title]=I just tested the accuracy of my watch&p[summary]=My watch is more accurate than {WatchPercentile}%25 of all tested watches on https://www.facebook.com/Toolwatch"
+          target="_blank">
+            <i class="fa fa-facebook" aria-hidden="true"></i>&nbsp;FACEBOOK
+
+          </a>
+
+          <a style="padding:15px; width:45%; margin-left:10px; color:white; background-color: #00a3e0" class="col-md-6 share-button"
+          href="https://twitter.com/intent/tweet?text=My watch is more accurate than {WatchPercentile}%25 of all tested watches on @ToolWatchApp&url=https://toolwatch.io"
+          target="_blank">
+            <i class="fa fa-twitter" aria-hidden="true"></i>&nbsp;TWITTER
+          </a>
+
+          <br>          <br>
+                    <br>
+                    <br>
+                    <br>
+          <a class="btn btn-success btn-lg no-display backToMeasure" href="/measures/">Back to measures</a>
+        </center>
+      </div>
+
+      <div class="wrapper col-md-4">
+          <div class="ribbon-wrapper-green"><div class="ribbon-green">More accurate than <span class="watch-percentile"></span>% <br> of all tested watches.</div></div>
+          <center>
+            <br>
+            <br>
+            <br>
+            <br>
+            <h2>Congratulations</h2>
+            <h3>
+              The accuracy of your <strong><span class="watch-brand"><?php echo $selectedWatch->brand;?></span></strong> is <span class="watch-accuracy"></span> spd.
+            </h3>
+          </center>
+
+          <canvas class="col-md-offset-1 col-md-10" id="canvas_animated_watch" width="250" height="250"></canvas>
+
+      </div>
+
+      <style media="screen">
+
+.wrapper{
+  background: white;
+border-radius: 10px;
+-webkit-box-shadow: 0px 0px 8px rgba(0,0,0,0.3);
+-moz-box-shadow:    0px 0px 8px rgba(0,0,0,0.3);
+box-shadow:         0px 0px 8px rgba(0,0,0,0.3);
+position: relative;
+z-index: 90;
+}
+
+      .ribbon-wrapper-green {
+    width: 227px;
+    height: 237px;
+    overflow: hidden;
+    position: absolute;
+    top: -3px;
+    right: -3px;
+  }
+
+  .ribbon-green {
+    font: bold 15px Sans-Serif;
+    color: #333;
+    text-align: center;
+    text-shadow: rgba(255,255,255,0.5) 0px 1px 0px;
+    -webkit-transform: rotate(45deg);
+    -moz-transform:    rotate(45deg);
+    -ms-transform:     rotate(45deg);
+    -o-transform:      rotate(45deg);
+    position: relative;
+    padding: 7px 0;
+    left: -5px;
+    top: 50px;
+    width: 320px;
+    background-color: #BFDC7A;
+    background-image: -webkit-gradient(linear, left top, left bottom, from(#BFDC7A), to(#8EBF45));
+    background-image: -webkit-linear-gradient(top, #BFDC7A, #8EBF45);
+    background-image:    -moz-linear-gradient(top, #BFDC7A, #8EBF45);
+    background-image:     -ms-linear-gradient(top, #BFDC7A, #8EBF45);
+    background-image:      -o-linear-gradient(top, #BFDC7A, #8EBF45);
+    color: #6a6340;
+    -webkit-box-shadow: 0px 0px 3px rgba(0,0,0,0.3);
+    -moz-box-shadow:    0px 0px 3px rgba(0,0,0,0.3);
+    box-shadow:         0px 0px 3px rgba(0,0,0,0.3);
+  }
+
+  .ribbon-green:before, .ribbon-green:after {
+    content: "";
+    border-top:   3px solid #6e8900;
+    border-left:  3px solid transparent;
+    border-right: 3px solid transparent;
+    position:absolute;
+    bottom: -3px;
+  }
+
+  .ribbon-green:before {
+    left: 0;
+  }
+  .ribbon-green:after {
+    right: 0;
+  }
+      </style>
+
+
+
+
+    </div>
+
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
 
@@ -45,59 +166,6 @@
 
                 <div class="form-group sync-success" style="display:none; text-align: center">
 
-                    <div class="col-sm-12">
-
-                        <canvas class="col-sm-4" id="canvas_animated_watch" width="250" height="250"></canvas>
-                        <div class="col-sm-8">
-
-                            <div class="col-sm-8">
-                                <h1>Congratulations!</h1> <br/> <p class="accuracy-subtitle"> The accuracy of your <strong><span class="watch-brand"><?php echo $selectedWatch->brand;?></span></strong> is </p>
-                            </div>
-                            <div class="col-sm-8">
-                                <strong><span class="watch-accuracy"></span> seconds a day!</strong>
-                            </div>
-                        </div>
-
-                        <div class="share-plugin col-md-offset-1 col-sm-10">
-                            <center><p><br /><br />Your watch is <b>more accurate than <span class="watch-percentile"></span>%</b> of all tested watches.</p></center>
-
-                        <div class="col-sm-12">
-                          <a class="share-button"
-                          href="https://twitter.com/intent/tweet?text=My watch is more accurate than {WatchPercentile}%25 of all tested watches on @ToolWatchApp&url=https://toolwatch.io"
-                          target="_blank">
-                            <img src="<?php echo base_url();?>assets/img/tweet.png" alt="Tweet on tweeter" />
-                          </a>
-
-                          <a class="share-button"
-                          href="http://www.facebook.com/sharer.php?s=100&p[url]=https://toolwatch.io&p[title]=I just tested the accuracy of my watch&p[summary]=My watch is more accurate than {WatchPercentile}%25 of all tested watches on https://www.facebook.com/Toolwatch"
-                          target="_blank">
-                            <img src="<?php echo base_url();?>assets/img/facebook.png" alt="Share on timeline" />
-                          </a>
-
-                          <a class="share-button"
-                          href="https://plus.google.com/share?url=https://toolwatch.io"
-                          target="_blank">
-                            <img src="<?php echo base_url();?>assets/img/google-plus.png" alt="Share on timeline" />
-                          </a>
-
-                          <a class="share-button"
-                          href="mailto:?to=&Subject=I just tested the accuracy of my watch&body=My watch is more accurate than {WatchPercentile}%25 of all tested watches on https://toolwatch.io"
-                          target="_top">
-                            <img src="<?php echo base_url();?>assets/img/email-share.png" alt="Share by email" />
-                          </a>
-                        </div>
-
-                        <br/>
-                        <br />
-                        <center><p><br /><br />We <i style="color:#4d77a7" class="fa fa-heart"></i> <a href="https://instagram.com/toolwatchapp/">Instagram</a>, tag us with your wristshots and share your results using #ToolwatchApp !</p></center>
-                        <br />
-                        <!-- SnapWidget -->
-                          <script src="http://snapwidget.com/js/snapwidget.js"></script>
-                          <iframe src="http://snapwidget.com/in/?u=dG9vbHdhdGNoYXBwfGlufDEyNXwzfDJ8fG5vfDV8bm9uZXxvblN0YXJ0fHllc3x5ZXM=&ve=290216" title="Instagram Widget" class="snapwidget-widget" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:100%;"></iframe>
-                        </div>
-
-                    </div>
-
                 </div>
 
 
@@ -108,7 +176,6 @@
                         <center>
                             <input type="hidden" name="measureId" value="<?php echo $measureId;?>">
                             <button class="btn btn-primary btn-lg" name="startSync">Check now!</button>
-                            <a class="btn btn-success btn-lg no-display backToMeasure" href="/measures/">Back to measures</a>
                         </center>
                     </div>
                 </div>
