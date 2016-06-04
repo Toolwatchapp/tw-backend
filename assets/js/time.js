e84adb72912a96a1cc652e7680c5308c9f8797d0
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-06-04 15:10:16 -0400

    use stratum 1 ntp sync as time reference

--- assets/js/time.js
+++ assets/js/time.js
@@ -1,25 +1,119 @@
-/**
- * The following code is adapted from http://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/
- */
+window.syncedDate = null;
+window.syncedDataAnchor = null;
+window.offset = null;
+
+(function(w){
+	var perfNow;
+	var perfNowNames = ['now', 'webkitNow', 'msNow', 'mozNow'];
+	if(!!w['performance']) for(var i = 0; i < perfNowNames.length; ++i)
+	{
+		var n = perfNowNames[i];
+		if(!!w['performance'][n])
+		{
+			perfNow = function(){return w['performance'][n]()};
+			break;
+		}
+	}
+	if(!perfNow)
+	{
+    if(Date.now) {
+        perfNow = Date.now();
+    } else {
+        perfNow = +(new Date());
+    }
+	}
+	w.perfNow = perfNow;
+})(window);
+
+var offsets = [];
+var counter = 0;
+var maxTimes = 10;
+var beforeTime = null;
+
+function median(offset){
+  offset.sort( function(a,b) {return a - b;} );
+
+  var half = Math.floor(offset.length/2);
+
+  if(offset.length % 2){
+    return offset[half];
+  } else {
+    return (offset[half-1] + offset[half]) / 2.0;
+  }
+}
+
+function getTimeDiff(){
+  beforeTime = window.perfNow();
+  $.ajax('/api/time', {
+      type: 'GET',
+      success: function(response) {
+          var now, timeDiff, serverTime, offset;
+          counter++;
+
+          // Get offset
+          now = window.perfNow();
+          timeDiff = (now-beforeTime)/2;
+
+          serverTime = response.time-timeDiff;
+          offset = Date.now()-serverTime;
+
+          // Push to array
+          offsets.push(offset)
+          if (counter < maxTimes) {
+            // Repeat
+            getTimeDiff();
+          } else {
+            var averageOffset = median(offsets);
+            window.syncedDate = new Date(Date.now()-averageOffset);
+            window.syncedDataAnchor = window.perfNow();
+            console.log(window.syncedDate);
+            console.log("average offset:" + averageOffset);
+            window.offset  = averageOffset;
+          }
+      }
+  });
+}
+
+function getAccurateTime(){
+  if(window.syncedDate === null){
+    getTimeDiff();
+    setTimeout(getAccurateTime, 5000);
+    console.log("waiting");
+    return;
+  }
+
+  window.syncedDate = new Date(
+    window.syncedDate.getTime() +
+    window.perfNow() - window.syncedDataAnchor
+  );
+
+  window.syncedDataAnchor = window.perfNow();
+
+  console.log(window.syncedDate);
+
+  return window.syncedDate;
+}
 
 function getTime() {
-  var t = new Date();
-  var seconds = t.getSeconds();
-  var minutes = t.getMinutes();
-  var hours = t.getHours();
-  var days = t.getDate();
-  var months = t.getMonth();
-  var years = t.getFullYear();
+
+  getAccurateTime();
+
   return {
-    'total': t,
-    'years':years,
-    'months':months,
-    'days': days + "<sup>" + nth(days) + "</sup>",
-    'hours': hours,
-    'minutes': minutes,
-    'seconds': seconds
+    'total': window.syncedDate,
+    'years':window.syncedDate.getFullYear(),
+    'months':window.syncedDate.getMonth(),
+    'days': window.syncedDate.getDate() + "<sup>" + nth(window.syncedDate.getDate()) + "</sup>",
+    'hours': window.syncedDate.getHours(),
+    'minutes': window.syncedDate.getMinutes(),
+    'seconds': window.syncedDate.getSeconds(),
+    'milliseconds': window.syncedDate.getMilliseconds(),
+    'offset':window.offset
   };
 }
+
+/**
+ * The following code is adapted and improved upon http://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/
+ */
 
 function nth(d) {
   if(d>3 && d<21) return 'th';
