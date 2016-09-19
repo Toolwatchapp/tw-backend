9b396174a0091042630be7a36ee7b2ed0dde2a60
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-09-19 14:51:52 -0500

    regenerate CSRF token HkOne 162107

--- application/config/config.php
+++ application/config/config.php
@@ -435,7 +435,7 @@ $config['csrf_protection']   = TRUE;
 $config['csrf_token_name']   = 'csrf_test_name';
 $config['csrf_cookie_name']  = 'csrf_cookie_name';
 $config['csrf_expire']       = 7200;
-$config['csrf_regenerate']   = FALSE;
+$config['csrf_regenerate']   = TRUE;
 $config['csrf_exclude_uris'] = array('hooks', 'api/[a-z\/]+');
 
 /*
