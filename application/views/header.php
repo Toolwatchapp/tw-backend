2f79a3f34fee25e6abcb634cf6f6f66cffcf89ea
Mathieu Nayrolles <MathieuNls@users.noreply.github.com>
2017-05-24 14:09:54 -0400

    fix(analytics): Force page send

--- application/views/header.php
+++ application/views/header.php
@@ -65,19 +65,19 @@
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="<?php echo ico_url('ms-icon-144x144.png')?>">
     <meta name="theme-color" content="#ffffff">
-	<script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];
-a.async=1;
-a.src=g;
-m.parentNode.insertBefore(a,m)
-          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-          ga('create', 'UA-59148878-1', 'auto', {'allowLinker': true});
-	  ga('require', 'linker');
-	  ga('linker:autoLink', ['toolwatchapp.io'] );
-    </script>
+ga('create', 'UA-59148878-1', 'auto', {'allowLinker': true});
+ga('require', 'linker');
+ga('linker:autoLink', ['toolwatchapp.io'] );
+ga('send', 'pageview');
+
+</script>
     <script type="application/ld+json">
     { "@context" : "http://schema.org",
       "@type" : "Organization",
